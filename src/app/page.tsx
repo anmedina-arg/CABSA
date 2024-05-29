ab476130209d842dbfee7b329fe3a692318619ca
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2024-05-28 22:02:08 -0300

    Created the hero component and added it to the page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import styles from './page.module.css';
-
+import { Hero } from '@/components/Hero';
 export default function Home() {
   return (
     <main className={styles.main}>
       <p>Hello World</p>
+      <Hero />
     </main>
   );
 }
