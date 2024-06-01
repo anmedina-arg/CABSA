2a7556884cb571b0aafba03a0829a9ebcfeedc65
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-01 09:27:41 -0300

    updating storybook

--- src/stories/Page.stories.ts
+++ src/stories/Page.stories.ts
@@ -2,31 +2,33 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { within, userEvent, expect } from '@storybook/test';
 
 import { Page } from './Page';
+import Home from '@/app/page';
+
 
 const meta = {
   title: 'Example/Page',
-  component: Page,
+  component: Home,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Page>;
+} satisfies Meta<typeof Home>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedOut: Story = {};
+export const CabsaPage: Story = {};
 
 // More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
-export const LoggedIn: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = canvas.getByRole('button', { name: /Log in/i });
-    await expect(loginButton).toBeInTheDocument();
-    await userEvent.click(loginButton);
-    await expect(loginButton).not.toBeInTheDocument();
+// export const LoggedIn: Story = {
+//   play: async ({ canvasElement }) => {
+//     const canvas = within(canvasElement);
+//     const loginButton = canvas.getByRole('button', { name: /Log in/i });
+//     await expect(loginButton).toBeInTheDocument();
+//     await userEvent.click(loginButton);
+//     await expect(loginButton).not.toBeInTheDocument();
 
-    const logoutButton = canvas.getByRole('button', { name: /Log out/i });
-    await expect(logoutButton).toBeInTheDocument();
-  },
-};
+//     const logoutButton = canvas.getByRole('button', { name: /Log out/i });
+//     await expect(logoutButton).toBeInTheDocument();
+//   },
+// };
