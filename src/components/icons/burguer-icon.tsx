a67d2e4c2f5b3fdcefea28a454f735f8a7fcfdcf
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-04 18:16:39 -0300

    FEAT-navbar movile

---
+++ src/components/icons/burguer-icon.tsx
@@ -0,0 +1,16 @@
+function BurguerIcon() {
+  return (
+    <svg fill="#ffffff" width="30px" height="20px" viewBox="0 0 27 24" xmlns="http://www.w3.org/2000/svg">
+      <g data-name="Layer 2">
+        <g data-name="menu">
+          <rect width="27" height="24" transform="rotate(180 12 12)" opacity="0"/>
+          <rect x="3" y="11" width="26" height="2" rx=".95" ry=".95"/>
+          <rect x="3" y="16" width="26" height="2" rx=".95" ry=".95"/>
+          <rect x="3" y="6" width="26" height="2" rx=".95" ry=".95"/>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+export default BurguerIcon;
---
+++ src/components/navbar/navbar-mobile.tsx
@@ -0,0 +1,34 @@
+import Anchor from '../anchor/anchor';
+import BurguerIcon from '../icons/burguer-icon';
+import CloseIcon from '../icons/close-icon';
+import './navbar.css';
+import { useState } from 'react';
+import { itemsNavbar, itemsNavbarProps } from './navbar';
+
+type NavBarMobileProps = {
+	itemsNavbar: itemsNavbarProps[]
+};
+
+function NavBarMobile({ itemsNavbar }:NavBarMobileProps) {
+  const [show, setShow] = useState(false);
+
+  function toogleMenu() {
+    setShow(!show);
+  }
+  return (
+    <div onClick={()=>setShow(!show)}>
+      <div onClick={toogleMenu}>
+        {show ? <CloseIcon /> : <BurguerIcon />}
+      </div>
+      {show && 
+        <div className="itemsBurguerMenu">
+          {
+            itemsNavbar.map(({ title, path }) => <Anchor key={title} to={path} label={title} setShow={setShow} />)
+          }
+        </div>
+      }
+    </div>
+  );
+}
+
+export default NavBarMobile;
