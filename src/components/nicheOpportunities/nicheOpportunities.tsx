f6761cb4071987bc5f944794e39443ef682b28cc
gabrielweht <gabrielestebanw@gmail.com>
2024-06-04 17:02:13 -0300

    feat niche opportunities section

---
+++ src/components/nicheOpportunities/nicheOpportunities.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import { Card } from '../cards/cards';
+import './nicheOpportunities.css';
+import { nicheCards } from '@/utils/CardsValues';
+
+export function NicheOpportunities() {
+  const width = '45rem';
+  const height = '20rem';
+
+  return (
+    <div className="divContainerNiche">
+      {nicheCards.map((card, key) => {
+        return (
+          <Card
+            key={key}
+            title={card.title}
+            width={width}
+            height={height}
+            flexDirection={card.flexDirection}
+            turnCard={card.turnCard}
+            paragraph={card.paragraph}
+          >
+            <Image src={card.icon} alt="icon" className='logo'/>
+          </Card>
+        );
+      })}
+    </div>
+  );
+}
