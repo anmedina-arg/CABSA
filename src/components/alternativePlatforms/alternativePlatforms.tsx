76cb3fc91a6c7c9b0fb9327099501774fa2c7f95
gabrielweht <gabrielestebanw@gmail.com>
2024-06-05 14:21:53 -0300

    feat alternative platforms

---
+++ src/components/alternativePlatforms/alternativePlatforms.tsx
@@ -0,0 +1,9 @@
+import './alternativePlatforms.css';
+
+export function AlternativePlatforms() {
+  return(
+    <p className="textStyles">
+      Taroko is an alternative platform for both large and niche funds. <br />The platform conducts thorough due diligence. <br />The fund holds only the alternative funds in which it has an interest in its portfolio. <br />It provides support when there is no PM in alternative investments.
+    </p>
+  );
+}
