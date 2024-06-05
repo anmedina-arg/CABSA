049671d0d1744b87cc76f693d9b1c3fdd8c766af
gabrielweht <gabrielestebanw@gmail.com>
2024-06-04 21:46:27 -0300

    Feat club deals service

---
+++ src/components/clubDeals/clubDeals.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import './clubDeals.css';
+import player from '@/assets/Player.webp';
+import Link from 'next/link';
+
+export function ClubDeals() {
+  return (
+    <div className='divContainerClubDeals'>
+      <div className='textButtonContainer'>
+        <p className='clubDealsText'>
+        We provide an exclusive service that allows you to acquire ownership stakes in elite sports teams from the world’s top leagues, along with the business opportunities that come with them.
+        </p>
+        <Link href={'#contact'} className='buttonLinkClubDeals'>Learn more</Link>
+      </div>
+      <Image src={player} alt='Player image' className='playerImage'/>
+    </div>
+  );
+}
