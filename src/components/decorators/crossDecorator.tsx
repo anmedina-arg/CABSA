f13c1fdcf587aa4bad16d9d5f887d9ef0b88b692
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-06 20:45:07 -0300

    TEMP-cross decorator

---
+++ src/components/decorators/crossDecorator.tsx
@@ -0,0 +1,14 @@
+import './crossDecorator.css';
+
+const CrossDecorator = () => {
+
+	return (
+		<div class='cossDecoratorContainer'>
+			<div class='bajolienzo'></div>
+			<div class='lienzo'></div>
+			<div class='sobrelienzo'></div>
+		</div>
+	);
+};
+
+export default CrossDecorator;
