61d7fba91523876e8ad6a8b5df3fb6beab886445
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-06 20:56:03 -0300

    FIX-scroll spy refered to contact section

--- src/sections/contact/contact.tsx
+++ src/sections/contact/contact.tsx
@@ -5,10 +5,10 @@ import contactImage from '@/assets/contactImage.webp';
 
 export function Contact() {
   return (
-    <section className="contactSection">
+    <section className="contactSection" id='contact'>
       <div className="divContainerContact" >
         <Image src={contactImage} alt="Contact image" className="imageContact" />
-        <h2 className="contactTitle" id='contact'>Contact us</h2>
+        <h2 className="contactTitle">Contact us</h2>
         <p className="contactText">
           Welcome to CABSA. We are here to assist you with all your financial
           needs. Please complete the form below, and a member of our team will
