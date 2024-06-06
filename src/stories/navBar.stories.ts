affe9364a4b1655de7220e54afe8b1030f73a335
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-06 10:16:52 -0300

    FIX-stories navbar

--- src/stories/navBar.stories.ts
+++ src/stories/navBar.stories.ts
@@ -1,8 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 import { Navbar } from '@/components/navbar/navbar';
 import { itemsNavbar } from '@/components/navbar/navbar';
-import mainLogo from '../assets/main-logo.png';
 
 const meta = {
   title: 'NavBar',
@@ -19,6 +17,5 @@ type Story = StoryObj<typeof meta>;
 export const NavBar: Story = {
   args: {
     items: itemsNavbar,
-    logo: mainLogo
   }
 };
