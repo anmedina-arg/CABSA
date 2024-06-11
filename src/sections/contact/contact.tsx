b546b182ae2099afce10aa1d8815cbe238f3a5f3
Gabriel Weht <gweht@maxionmontich.local>
2024-06-11 16:36:29 -0300

    fix services, contact and footer styles

--- src/sections/contact/contact.tsx
+++ src/sections/contact/contact.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image';
 import './contact.css';
 import { ContactForm } from '@/components/contactForm/contactForm';
 import contactImage from '@/assets/contactImage.webp';
+import MainTitle from '@/components/mainTitle/mainTitle';
 
 export function Contact() {
   return (
     <section className="contactSection" id='contact'>
       <div className="divContainerContact" >
         <Image src={contactImage} alt="Contact image" className="imageContact" />
-        <h2 className="contactTitle">Contact us</h2>
+        <div className='divContainerTitleContactUs'>
+          <MainTitle title="contact us" />
+        </div>
         <p className="contactText">
           Welcome to CABSA. We are here to assist you with all your financial
           needs. Please complete the form below, and a member of our team will
