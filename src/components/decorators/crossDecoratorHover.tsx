c027f3a257d58060bd56e418be47348c8d6fc6b8
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-13 12:16:50 -0300

    adding cross decorator with hover effect

---
+++ src/components/decorators/crossDecoratorHover.tsx
@@ -0,0 +1,80 @@
+import './crossDecoratorHover.css';
+
+const CrossDecoratorHover = () => {
+  return (
+    <div className="crossDecoratorHoverContainer">
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+
+      </div>
+      <div className="crossDecoratorHoverMinContainer">
+        <div className="crossHover">+</div>
+        <div className="crossHover">+</div>
+
+      </div>
+    </div>
+  );
+};
+
+export default CrossDecoratorHover;
