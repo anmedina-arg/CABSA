1e620659fa41ffbec008265b326e3b1540b00b57
anmedina-arg <andres.medina.arg@gmail.com>
2024-06-14 14:40:26 -0300

    change position relative cross decorator

--- src/sections/contact/contact.tsx
+++ src/sections/contact/contact.tsx
@@ -5,22 +5,31 @@ import { ContactForm } from '@/components/contactForm/contactForm';
 import contactImage from '@/assets/contactImage.webp';
 import MainTitle from '@/components/mainTitle/mainTitle';
 import { useEffect, useState } from 'react';
+import CrossDecoratorHover from '@/components/decorators/crossDecoratorHover';
 
 export function Contact() {
   const [isPortrait, setIsPortrait] = useState(false);
 
   useEffect(() => {
-    if (window.innerWidth <= 768 ) setIsPortrait(true);
+    if (window.innerWidth <= 768) setIsPortrait(true);
     else setIsPortrait(false);
   }, []);
 
   return (
     <section className="contactSection" id={isPortrait ? undefined : 'contact'}>
+      <div className="crossDecorator">
+        <CrossDecoratorHover />
+      </div>
       <div className="divContainerContact" >
         <Image src={contactImage} alt="Contact image" className="imageContact" />
-        <div className='divContainerTitleContactUs' id= {isPortrait ? 'contact' : undefined}>
+        <div className='divContainerTitleContactUs' id={isPortrait ? 'contact' : undefined}>
           <MainTitle title="contact us" />
         </div>
+      </div>
+      <div className="divContainerContact" id='contact'>
+        <header>
+          <h3 className="contactTitle">Contact Us</h3>
+        </header>
         <p className="contactText">
           Welcome to CABSA. We are here to assist you with all your financial
           needs. Please complete the form below, and a member of our team will
