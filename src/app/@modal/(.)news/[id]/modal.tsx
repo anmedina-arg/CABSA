475decd02cd012f10f476d75439a6ed26730a625
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2024-06-18 21:34:45 -0300

    Added the @modal slot and (.)news/ intercepting route, to render news entries in the current page

---
+++ src/app/@modal/(.)news/[id]/modal.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ElementRef, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import './modal.css';
+export function Modal({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const dialogRef = useRef<ElementRef<'dialog'>>(null);
+
+  function onDismiss() {
+    router.back();
+  }
+
+  useEffect(() => {
+    if (!dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+    }
+  });
+
+  return createPortal(
+    <div className="modal-backdrop">
+      <dialog ref={dialogRef} className="modal" onClose={onDismiss}>
+        {children}
+        <button onClick={onDismiss} className="close-button"></button>
+      </dialog>
+    </div>,
+    document.getElementById('modal-root')!
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,23 +6,26 @@ const roboto = Roboto({
   subsets: ['latin'],
   weight: ['300', '500', '700', '900'],
   style: ['italic', 'normal'],
-  display: 'swap'
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
   title: 'CABSA',
-  description: 'CABSA - We offer empathetic, personalized and impactful financial solutions.',
+  description:
+    'CABSA - We offer empathetic, personalized and impactful financial solutions.',
 };
 
-export default function RootLayout({
-  children,
-}: {
+export default function RootLayout(props: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
-      <body className={`atTop ${roboto.className}`}>{children}</body>
+      <body className={`atTop ${roboto.className}`}>
+        {props.children}
+        {props.modal}
+        <div id="modal-root" />
+      </body>
     </html>
   );
 }
