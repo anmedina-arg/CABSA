eb5da53c46a96f1cbf5b0b78ba71f27c5e3c7305
Nicolas Marinelli <nicolasmarinelli@protonmail.com>
2024-07-02 21:04:55 -0300

    Removed the function to check if the device is a mobile device

--- src/sections/blog/BlogCard.tsx
+++ src/sections/blog/BlogCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
-import { ReactNode, forwardRef, useEffect, useState } from 'react';
+import { ReactNode, forwardRef } from 'react';
 import { GoArrowUpRight } from 'react-icons/go';
 
 export type NewsEntry = {
@@ -12,26 +12,6 @@ export type NewsEntry = {
 };
 
 function BlogCard(props: NewsEntry, ref: React.Ref<HTMLDivElement>): ReactNode {
-  const [isMobile, SetIsMobile] = useState(false);
-  useEffect(() => {
-    if ('maxTouchPoints' in window.navigator) {
-      SetIsMobile(navigator.maxTouchPoints > 0);
-    } else {
-      const mQ = matchMedia?.('(pointer:coarse)');
-      if (mQ?.media === '(pointer:coarse)') {
-        SetIsMobile(!!mQ.matches);
-      } else if ('orientation' in window) {
-        SetIsMobile(true); // deprecated, but good fallback
-      } else {
-        // Only as a last resort, fall back to user agent sniffing
-        const UA = navigator.userAgent;
-        SetIsMobile(
-          /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-      }
-    }
-  }, []);
   return (
     <article ref={ref} id={`news-${props.id}`} className="blogCard">
       <div className="blogImageContainer">
@@ -39,11 +19,7 @@ function BlogCard(props: NewsEntry, ref: React.Ref<HTMLDivElement>): ReactNode {
       </div>
       <div className="blogInfoContainer">
         <h3>{props.title}</h3>{' '}
-        <Link
-          href={`/news/${props.slug}.pdf`}
-          passHref
-          // target={isMobile ? '_blank' : '_self'}
-        >
+        <Link href={`/news/${props.slug}.pdf`} passHref>
           Read More <GoArrowUpRight />
         </Link>
       </div>
